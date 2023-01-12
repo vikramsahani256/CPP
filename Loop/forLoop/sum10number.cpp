abc33b00bc0ad8d8be3aacc8624f60be087c4aaf
Vikram Sahani <vikramsahani256@gmail.com>
2023-01-12 23:35:50 +0530

    for loop program

---
+++ Loop/forLoop/sum10number.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int i,n=10,sum=0;
+for(i=0;i<=n;i++){
+  sum=sum+i;
+}
+cout<<"Sum of first "<<n<<" number is "<<sum<<endl;
+  return 0;
+}
